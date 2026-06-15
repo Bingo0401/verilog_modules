f8c72292776de25efcc5fab97eeaed6723cedbd1
Bingo <bingoenping@gmail.com>
2026-06-16 05:27:35 +0800

    added four bit ripple carry adder

---
+++ Adders/four_bit_adders/ripple_carry_adder/four_bit_ripple_carry_adder.v
@@ -0,0 +1,31 @@
+module full_adder(A, B, Cin, Cout, S);
+input A, B, Cin;
+output Cout, S;
+
+xor X1(S, A, B, Cin);
+
+assign Cout = (A & B)|(Cin & A)|(B & Cin);
+endmodule
+
+
+
+module four_bit_carry_ripple_adder(Cin, Cout, A, B, S);
+
+
+input Cin;
+input [3:0] A;
+input [3:0] B;
+output Cout;
+output [3:0] S;
+wire C0, C1, C2;
+
+
+full_adder FA0(.A(A[0]), .B(B[0]), .Cin(Cin), .Cout(C0), .S(S[0]));
+full_adder FA1(.A(A[1]), .B(B[1]), .Cin(C0), .Cout(C1), .S(S[1]));
+full_adder FA2(.A(A[2]), .B(B[2]), .Cin(C1), .Cout(C2), .S(S[2]));
+full_adder FA3(.A(A[3]), .B(B[3]), .Cin(C2), .Cout(Cout), .S(S[3]));
+
+
+endmodule
+
+//Coded by Bingo
